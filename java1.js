88552c5420269ae122660161a42a2c2e3f4c5a04
muhammed jaseer mc <jaseermc786@gmail.com>
2023-06-14 11:07:54 +0530

    javamarkgrade

---
+++ java1.js
@@ -0,0 +1,18 @@
+let i=11
+
+if(i>80 && i<=100){
+console.log("grade A")
+}
+else if(i>60 && i<=80){
+    console.log("grade B")
+}
+else if(i>40 && i<=60){
+    console.log("grade C")
+}
+else if(i>20 && i<=40){
+    console.log("grade D")
+}
+else if(i>1 && i<=20){
+    console.log("faild")
+}
+
